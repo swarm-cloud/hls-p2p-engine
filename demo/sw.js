6f1b78a37adc7ecaf65b0fc18441fb801d0bbf7b
snowinszu <86755838@qq.com>
2024-11-25 13:03:37 +0800

    update demo

--- demo/sw.js
+++ demo/sw.js
@@ -1,3 +1,8 @@
 self.importScripts('https://cdn.jsdelivr.net/npm/@swarmcloud/hls/hls-proxy.js')
 
-
+// uncomment code below to customize HlsProxy config, see https://www.swarmcloud.net/web-hls/API#hlsproxy-api
+// new HlsProxy({
+//     insertTimeOffsetTag: true,
+//     allowedPlaylistSuffix: ['m3u8'],     // add other playlist suffix like 'html' if your playlist url is xxx.html
+//     allowedMediaFiles: ['ts', 'mp4', 'm4s', 'fmp4'], // add other media file suffix like 'txt' if your media file url is xxx.txt
+// })
